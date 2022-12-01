1133e09d1575d956c9bd3d269c7e4b0e80cb4cc4
itaquito <ichilaquiles@gmail.com>
2022-11-30 23:33:27 -0600

    Updates template

--- TEMPLATE/index.tsx
+++ TEMPLATE/index.tsx
@@ -1,1 +1,9 @@
-const hola: string = "s";
+const fs = require("fs/promises");
+
+async function main() {
+  const data = await fs.readFile("./input.txt", { encoding: "utf8" });
+
+  const lines: string[] = data.split("\r\n");
+}
+
+main();
